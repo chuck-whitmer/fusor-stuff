4f362f51bd1a0cf3e58b7e65627b792c6ebbf0d9
Chuck Whitmer <chuck-whitmer@users.noreply.github.com>
2019-07-26 14:34:47 -0700

    Arduino app working from laptop

--- VoltageMonitor/VoltageMonitor.ino
+++ VoltageMonitor/VoltageMonitor.ino
@@ -3,33 +3,33 @@
 
 #include <Wire.h>
 
-
 U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0); 
 
-// End of constructor list
-
-int height;
-int width;
-int maxCharHeight;
+int displayHeight;
+int displayWidth;
+int lineHeight;
+int variacAdcPin = 0;
 
 void setup(void) {
   u8g2.begin();
-  u8g2.setFont(u8g2_font_ncenB08_tr); // choose a suitable font
-  height = u8g2.getDisplayHeight();
-  width = u8g2.getDisplayWidth();
-  maxCharHeight = u8g2.getMaxCharHeight();
+  u8g2.setFont(u8g2_font_amstrad_cpc_extended_8r);
+  u8g2.setFontPosTop();
+  displayHeight = u8g2.getDisplayHeight();
+  displayWidth = u8g2.getDisplayWidth();
+  lineHeight = displayHeight/4;
+  analogReference(INTERNAL1V1);
 }
 
-char buffer[20];
+char buf[20];
 
 void loop(void) {
   u8g2.clearBuffer();         // clear the internal memory
-  for (int i=1; i<=4; i++)
+  for (int i=0; i<4; i++)
   {
-    int row = i*maxCharHeight-1;
-    u8g2.drawStr(0,row,itoa(maxCharHeight,buffer,10));
-    u8g2.drawStr(64,row,itoa(width,buffer,10));
-    u8g2.drawStr(96,row,itoa(height,buffer,10));
+    int row = i*lineHeight;
+    u8g2.drawStr(0,row,itoa(maxCharHeight,buf,10));
+    u8g2.drawStr(64,row,itoa(width,buf,10));
+    u8g2.drawStr(96,row,itoa(height,buf,10));
   }
   u8g2.sendBuffer();          // transfer internal memory to the display
   delay(1000);  
