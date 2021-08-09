1f60c48084c1db13c666d09fa2d044e1c4cb75c6
Charles Whitmer <chuck@whitmer.us>
2021-08-09 08:29:58 -0700

    First steps in SinFitter

---
+++ Arduino/SinFitter/FastTrig.h
@@ -0,0 +1,15 @@
+class FastTrig
+{
+public:  
+  static float Cos(long x); // x is 16 bit fixed point number of rotations.
+  static float Sin(long x)
+  {
+    return Cos(x + 3*16384L); // Just add 0.11b to the fixed point argument, moving 3 quadrants.
+  }
+  static void Test();  
+private:
+  FastTrig()
+  {}
+  static const long b; // Magic number for InnerCos.
+  static long InnerCos(long x); // x is 14 bit positive fraction of a quadrant. (0 <= x <= 16384)
+};
---
+++ Arduino/SinFitter/FastTrig.ino
@@ -0,0 +1,44 @@
+void FastTrig::Test()
+{
+  char buf[100];
+  char cbuf[15];
+  char sbuf[15];
+  for (int angle=0; angle<=720; angle+=5)
+  {
+    long rotations = angle*65536L/360;
+    float c = Cos(rotations);
+    float s = Sin(rotations);
+    dtostrf(c,10,5,cbuf);
+    dtostrf(s,10,5,sbuf);
+    sprintf(buf,"%3d %s %s\n",angle,sbuf,cbuf);
+    Serial.print(buf);
+  }
+}
+
+// x is 16 bit fixed point number of rotations.
+float FastTrig::Cos(long x) 
+{
+  x = x & 65535; // Integer number of rotations is irrelevant.
+  long z = x & 16383L; // 14 bit fixed point fraction of quadrant.
+  int q = x >> 14; // Quadrant number.
+
+  if (q & 1) z = 16384L - z;
+  long r = InnerCos(z);
+  if (q==1 || q==2) r = -r;
+  return r/65536.0f;
+}
+
+// Fast approximation to cosine. Accurate to +-0.0009.
+// Input x is 14 bit positive fraction of a quadrant. (0 <= x <= 16384)
+// Output is 16 bit fixed point.
+
+const long FastTrig::b = 14682; // 2^16 * 0.22402317
+
+long FastTrig::InnerCos(long x)
+{
+  // (1-x2)*(1-b*x2)
+  long x2 = (x*x+4096L)>>13; // 15 bit fixed.
+  long right = 32768L - ((b*x2+32768L)>>16);
+  long left = 32768L - x2;
+  return (right*left+8192L)>>14; // Returns 16 bit fixed.
+}
